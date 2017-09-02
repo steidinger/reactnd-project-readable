eff7b621b49611f4e4565e17f92e89b0e0d90e23
Frank Steidinger <steidinger@acm.org>
2017-09-02 11:30:06 +0200

    feature: add banner with link to main page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router';
+import { Link } from 'react-router-dom';
 import IndexView from './components/IndexView';
 import PostDetailsView from './components/PostDetailsView';
 
@@ -9,6 +10,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className="banner">
+          <Link to="/">Home</Link>
+        </div>
         <Switch>
           <Route path='/' exact component={IndexView} />
           <Route path='/:category' exact component={IndexView} />
--- frontend/src/components/CategoriesMenu.js
+++ frontend/src/components/CategoriesMenu.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
 import { NavLink } from 'react-router-dom';
 import './Categories.css';
 
 const CategoriesMenu = ({ categories }) => (
-    <ul className='category-list'>
-        <li key="all" className="category">
-            <NavLink to="/">Show All</NavLink>
-        </li>
+    <ul className="category-menu">
         {categories.map(({ name, path }) => (
-            <li key={path} className="category">
-                <NavLink to={`/${path}`}>{name}</NavLink>
+            <li key={path} className="category-menu-item">
+                <NavLink to={`/${path}`}>
+                    {name}
+                </NavLink>
             </li>
         ))}
     </ul>);
 
 const mapStateToProps = ({ categories }) => ({ categories });
 
-export default connect(mapStateToProps)(CategoriesMenu);
+export default withRouter(connect(mapStateToProps)(CategoriesMenu));
